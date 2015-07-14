43206c7b8c2f9a1d5a43e840d63b51691010e58b
Zachary Lester <zachary.greg.lester@gmail.com>
2015-07-13 20:16:24 -0700

    (client) Create temporary body styling and add NavBarBot

--- client/components/app/app.js
+++ client/components/app/app.js
@@ -1,8 +1,11 @@
 // app.js
-
+ContainerStyle = {
+  'minHeight': '100%'
+}
 React.render(
-  <div>
+  <div style={ContainerStyle}>
     <NavBarTop />
+    <NavBarBot />
   </div>,
   document.getElementsByClassName('appWrapper')[0]
 );
--- client/components/navBarBot/navBarBot.js
+++ client/components/navBarBot/navBarBot.js
@@ -1,1 +1,21 @@
-// navBarTop.js
+// navBarBot.js
+
+var Navbar = ReactBootstrap.Navbar;
+var Nav = ReactBootstrap.Nav;
+
+var NavBarBotStyle = {
+  position: "absolute",
+  bottom: "0",
+  left: "0",
+  width: '100%',
+  margin: '0 0 0 0'
+};
+
+var NavBarBot = React.createClass({
+  render: function() {
+    return (
+      <Navbar style={NavBarBotStyle} inverse brand='Built for Hack Reactor by Scintillating Acai, HR28, July 2015'>
+      </Navbar>
+    );
+  }
+});
