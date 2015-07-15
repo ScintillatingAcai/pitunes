69da9f1b1799a0b4a8ceaa1da4541cd5e83f22b8
Josh Tepei <joshuatepei94@gmail.com>
2015-07-14 19:25:29 -0700

    (client) Added beginning youtube functionality in player.js

--- client/components/video/player.js
+++ client/components/video/player.js
@@ -1,20 +1,14 @@
 // videoPlayer.js
 
 var VideoPlayerStyle = {
-
   width: "100%",
   height: "325"
 };
 
-
-
 var VideoPlayer = React.createClass({
- render: function() {
-   return (
-     <iframe style={VideoPlayerStyle} src="http://player.vimeo.com/video/54989781?api=1&player_id=player1" frameBorder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-   );
- }
+  render: function() {
+    return (
+      <iframe style={VideoPlayerStyle} src="https://www.youtube.com/embed/2HQaBWziYvY" frameBorder="0" ></iframe>    
+    );
+  }
 });
-
-
-// <video style={VideoPlayerStyle} width="614" height="350" src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4" autoPlay loop></video>
