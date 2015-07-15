fc98a2e7d550ff29836decbc7d59529325c61390
Josh Tepei <joshuatepei94@gmail.com>
2015-07-14 20:16:40 -0700

    (client) Disabled pause feature

--- client/components/video/player.js
+++ client/components/video/player.js
@@ -2,13 +2,14 @@
 
 var VideoPlayerStyle = {
   width: "100%",
-  height: "300px"
+  height: "300px",
+  pointerEvents: 'none'
 };
 
 var VideoPlayer = React.createClass({
   render: function() {
     return (
-      <div className="videoContainer"><iframe style={VideoPlayerStyle} src="https://www.youtube.com/embed/2HQaBWziYvY?autoplay=1&controls=0&showInfo=0&disablekb=1&enablejsapi=1&rel=0&modestbranding=1" frameBorder="0" ></iframe></div>    
+      <div className="videoContainer"><iframe style={VideoPlayerStyle} src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0&showInfo=0&disablekb=1&enablejsapi=1&rel=0&modestbranding=1" frameBorder="0" ></iframe></div>    
     );
   }
 });
