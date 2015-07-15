87f2c30662c48189e2972f4ba3295320c070ab4b
Josh Tepei <joshuatepei94@gmail.com>
2015-07-15 11:11:27 -0700

    (client) Modularized the rightContainer w/ other minor style changes

--- client/components/app/rightContainer.js
+++ client/components/app/rightContainer.js
@@ -1,14 +1,3 @@
-var RightContainerStyle = {
-  position: 'absolute',
-  right: 0,
-  minHeight: '90%',
-  width: '25%',
-  margin: '1px 0px 0px 0px',
-  backgroundColor: '#222222',
-  border: '2px solid #444444',
-  borderRadius: '2px'
-};
-
 var UserBox = React.createClass({
   render: function() {
     return (
@@ -19,8 +8,18 @@ var UserBox = React.createClass({
 
 var RightContainer = React.createClass({
   render: function() {
+    var style = {
+      position: 'absolute',
+      right: '0',
+      minHeight: '90%',
+      width: '25%',
+      margin: '1px 0px 0px 0px',
+      backgroundColor: '#222222',
+      border: '2px solid #444444',
+      borderRadius: '2px'
+    };
     return (
-      <div className="rightContainer" style={RightContainerStyle}>
+      <div className="rightContainer" style={style}>
         <UserBox />
       </div>
     );
