56c1c4d926cf477847a1b79a15fd56210bf96cf9
Josh Tepei <joshuatepei94@gmail.com>
2015-07-15 12:12:05 -0700

    (client) Disabled all page highlighting

--- client/components/app/app.js
+++ client/components/app/app.js
@@ -1,10 +1,28 @@
+var Styling = React.createClass({
+  render: function(){
+    var style = {
+      '-webkit-touch-callout': 'none',
+      '-webkit-user-select': 'none',
+      '-khtml-user-select': 'none',
+      '-moz-user-select': 'none',
+      '-ms-user-select': 'none',
+      'user-select': 'none'
+    };
+    return (
+      <div style={style}>
+        <NavBarTop />
+        <LeftContainer />
+        <CenterContainer />
+        <RightContainer />
+        <NavBarBot />
+      </div>
+    );
+  }
+})
+
 React.render(
   <div>
-    <NavBarTop />
-    <LeftContainer />
-    <CenterContainer />
-    <RightContainer />
-    <NavBarBot />
+    <Styling />
   </div>,
   document.getElementsByClassName('appWrapper')[0]
 );
