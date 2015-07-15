600f45f5307ceca639f1ac31f6936aadce17ebb3
Zachary Lester <zachary.greg.lester@gmail.com>
2015-07-14 17:26:38 -0700

    (client) Update mediaAdd placeholder

--- client/components/mediaAdd/mediaAdd.js
+++ client/components/mediaAdd/mediaAdd.js
@@ -3,11 +3,7 @@
 var MediaAdd = React.createClass({
   render: function() {
     return (
-      <div id="buttons">
-            <label> <input id="query" value='cats' type="text"/><button id="search-button" disabled onclick="search()">Search</button></label>
-          </div>
-      <div id="search-container">
-      </div>
+      <div>Placeholder</div>
     );
   }
 });
