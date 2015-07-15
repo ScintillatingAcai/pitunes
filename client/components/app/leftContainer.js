96134756821ff11f72ae70551db7c233b340b0c9
Josh Tepei <joshuatepei94@gmail.com>
2015-07-15 11:05:18 -0700

    (client) Modulized the leftContainer

--- client/components/app/leftContainer.js
+++ client/components/app/leftContainer.js
@@ -1,21 +1,35 @@
-var LeftContainerStyle = {
-  position: 'absolute',
-  left: '0',
-  minHeight: '90%',
-  width: '25%',
-  margin: '1px 0px 0px 0px',
-  backgroundColor: '#222222',
-  border: '2px solid #444444',
-  borderRadius: '2px'
+var PlaylistSavedBox = React.createClass({
+  render: function() {
+    return (
+      <PlaylistSaved />
+    );
+  }
+});
 
-};
+var MediaAddBox = React.createClass({
+  render: function() {
+    return (
+      <MediaAdd />
+    );
+  }
+});
 
 var LeftContainer = React.createClass({
   render: function() {
+    var style = {
+      position: 'absolute',
+      left: '0',
+      minHeight: '90%',
+      width: '25%',
+      margin: '1px 0px 0px 0px',
+      backgroundColor: '#222222',
+      border: '2px solid #444444',
+      borderRadius: '2px'
+    };
     return (
-      <div className="leftContainer" style={LeftContainerStyle}>
-        <PlaylistSaved />
-        <MediaAdd />
+      <div style={style}>
+        <PlaylistSavedBox />
+        <MediaAddBox />
       </div>
     );
   }
