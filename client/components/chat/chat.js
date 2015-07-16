de9f25fc24b3220fb97e406c5b4050fb19c01974
Josh Tepei <joshuatepei94@gmail.com>
2015-07-15 18:59:52 -0700

    (client) Implemented chat feature

--- client/components/chat/chat.js
+++ client/components/chat/chat.js
@@ -1,25 +1,26 @@
-var Messages = React.createClass({
+
+var ChatList = React.createClass({
   render: function() {
     var style = {
-      background: '#222222',
-      border: '2px solid #444444',
-      position: 'absolute', 
-      width: '100%',
-      height: '50%',
-      overflow: 'auto',
-      resize: 'vertical',
-      bottom: '10%',
-      color: 'blue'
+      marginTop: '5%',
+      color: 'grey',
+      listStyleType: 'none',
+      maxHeight: '200px'
     };
-    return (
-      <div>
-        Hello I am a message!!!
-      </div>
-    );
+    var createItem = function(itemText, index) {
+      return <li key={index + itemText}>{itemText}</li>;
+    };
+    return <ul style={style}>{this.props.items.map(createItem)}</ul>;
   }
 });
 
-var SubmitForm = React.createClass({
+var Chat = React.createClass({
+  getInitialState: function() {
+    return {items: [], text: ''};
+  },
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
   handleSubmit: function(e) {
     e.preventDefault();
     /** 
@@ -27,49 +28,52 @@ var SubmitForm = React.createClass({
     MAKE THE VARIABLE NAME EQUAL TO THE CURRENT USER
     ************************************************
     */
-    var name = 'Josh';
-    var message = React.findDOMNode(this.refs.message).value.trim();
-    console.log('message is ' + message);
-    if (!message) return;
-    
-    React.findDOMNode(this.refs.message).value = '';
+    var name = "Josh";
+    if (this.state.text === '') { return; }
+    var nextItems = this.state.items.concat([name + ': ' + this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+    var container = this.refs.messageContainer.getDOMNode();
+
+    if (container.scrollHeight - (container.scrollTop + container.offsetHeight) >= 50) {
+      this.scrolled = true;
+    } else {
+      this.scrolled = false;
+    }
+    if (!this.scrolled) { container.scrollTop = container.scrollHeight - 10; }
   },
   render: function() {
     var style = {
       left: '2.5%',
       width: '95%',
-      bottom: '5%',
-      position: 'absolute'
     };
-    var SearchBarInputStyle = {
-      backgroundColor: '#AAAAAA',
-      borderColor: '#EEEEEE'
-    };
-    return (
-       <form style={style}>
-         <input style={SearchBarInputStyle} type="text" className="form-control" placeholder="Say Something..." ref="message" />
-         <input onClick={this.handleSubmit} type="submit" hidden/>
-       </form>
-    );
-  }
-});
-
-var Chat = React.createClass({
-  render: function() {
-    var style = {
+    var divStyle = {
       background: '#222222',
       border: '2px solid #444444',
       position: 'absolute', 
       width: '100%',
       height: '50%',
-      overflow: 'auto',
-      resize: 'vertical',
       bottom: '0'
     };
+    var searchBarInputStyle = {
+      backgroundColor: '#AAAAAA',
+      borderColor: '#EEEEEE',
+      position: 'fixed',
+      bottom: '5%',
+      width: '49%'
+    };
+    var chatListStyle = {
+      bottom: '12%',
+      overflow: 'scroll'
+    };
     return (
-      <div style={style}>
-        <Messages />
-        <SubmitForm />
+      <div style={divStyle}>
+        <div style={chatListStyle} ref="messageContainer">
+          <ChatList items={this.state.items} />
+        </div>
+        <form style={style} onSubmit={this.handleSubmit}>
+          <input style={searchBarInputStyle} onChange={this.onChange} value={this.state.text} className="form-control" placeholder="Say Something..."/>
+        </form>
       </div>
     );
   }
