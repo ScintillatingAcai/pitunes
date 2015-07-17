4497bb41e39d9e67ba6f79e1d7d13b6c5a8d9253
Zachary Lester <zachary.greg.lester@gmail.com>
2015-07-17 12:24:19 -0700

    (client) Add socket event listener, extend video and search API

--- client/components/app/app.js
+++ client/components/app/app.js
@@ -1,7 +1,4 @@
-var user = null, 
-    room = 'root', 
-    server_uri = 'http://' + document.domain + ':3000'; 
-    socket = io(server_uri);
+// app.js
 
 var AppContainer = React.createClass({
   render: function(){
