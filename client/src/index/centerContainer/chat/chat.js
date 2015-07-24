8988008cc76340dc398a0272fdf1bd47225102bc
futbalguy <kyle.rokita@gmail.com>
2015-07-24 15:33:43 -0700

    updated message username

--- client/src/index/centerContainer/chat/chat.js
+++ client/src/index/centerContainer/chat/chat.js
@@ -41,7 +41,7 @@ var Chat = React.createClass({
     MAKE THE VARIABLE NAME EQUAL TO THE CURRENT USER
     ************************************************
     */
-    var name = user.display_name;
+    var name = app.get('user').get('display_name');
     if (this.state.text === '') { return; }
     socket.emit('user message', {displayName: name, message: this.state.text});
     var nextItems = this.state.items.concat([name + ': ' + this.state.text]);
