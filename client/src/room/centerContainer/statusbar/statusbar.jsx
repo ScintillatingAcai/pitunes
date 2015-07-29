b373f7c7130cdd5a3b1944ada1b5dec9abd7f050
Zachary Lester <zachary.greg.lester@gmail.com>
2015-07-29 10:50:13 -0700

    (client) Add statusbar, fix this ref in mediaAdd

---
+++ client/src/room/centerContainer/statusbar/statusbar.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+var $ = require('jquery');
+
+var server_uri = 'http://' + document.domain + ':3000',
+socket = io(server_uri);
+
+var StatusBar = React.createClass({
+  getInitialState: function () {
+    return {dj: this.props.dj, title: this.props.title};
+  },
+  componentDidMount: function () {
+
+  },
+  generateStatusMessage: function () {
+    if (this.state.dj === 'None') {
+      return 'No Song Currently Playing';
+    } else {
+      return this.state.dj + ' is playing ' + this.state.title;
+    }
+  },
+  render: function () {
+    var style = {
+      color: 'white',
+      minHeight: '30px',
+      borderBottom: '2px solid #e7e7e7'
+    };
+    return (
+      <div>
+      <h4 className='statusbarContainer text-center' style={style}>
+        {this.generateStatusMessage()}
+      </h4>
+      </div>
+    );
+  }
+});
+
+module.exports = StatusBar;
