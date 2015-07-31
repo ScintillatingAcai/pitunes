be73381fcb97caad379a9978c9b713679ed30a77
John Mai <maicorp@gmail.com>
2015-07-30 20:41:30 -0700

    (cleanup) cleanup package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var concat = require('gulp-concat');
 // var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
+// var sh = require('shelljs');
 var del = require('del');
 var jshint = require('gulp-jshint');
 var buffer = require('vinyl-buffer');
@@ -60,7 +60,7 @@ gulp.task('browserify-client', function (cb) {
    .pipe(source('bundle-client.min.js'))
    .pipe(buffer())
    // .pipe(uglify())
-   .pipe(gulp.dest(paths.dist + '/client'));
+   .pipe(gulp.dest(paths.dist));
 });
 
 
@@ -83,21 +83,21 @@ gulp.task('watch', function() {
   gulp.watch(paths.clientjs, ['browserify-client']);
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
+// gulp.task('git-check', function(done) {
+//   if (!sh.which('git')) {
+//     console.log(
+//       '  ' + gutil.colors.red('Git is not installed.'),
+//       '\n  Git, the version control system, is required to download Ionic.',
+//       '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+//       '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+//     );
+//     process.exit(1);
+//   }
+//   done();
+// });
 
 gulp.task('install_lib', function() {
-  return gulp.src(['./bower.json', './package.json'])
+  return gulp.src(['./package.json'])
   .pipe(install());
 });
 
