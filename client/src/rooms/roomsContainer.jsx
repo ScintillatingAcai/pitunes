9779ad4626d907be9a0ab294bb1e8b10493ff009
Zachary Lester <zachary.greg.lester@gmail.com>
2015-08-01 12:17:21 -0700

    (client) Change rooms styling/max image size

--- client/src/rooms/roomsContainer.jsx
+++ client/src/rooms/roomsContainer.jsx
@@ -113,7 +113,7 @@ var Rooms = React.createClass({
                 <div className="clearfix"></div>
                 <h2 className="section-heading j-center-text j-roomDisplay">{room.get('name')} <br />Current DJs: {room.get('usersCount')}</h2>
                 <div className="j-left-25">
-                  <img className="img-responsive j-max-height-216px j-room-image-display" src={room.get('videoURL')} alt="" />
+                  <img className="img-responsive z-img-bound j-room-image-display" src={room.get('videoURL')} alt="" />
                 </div>
               </div>
             )
