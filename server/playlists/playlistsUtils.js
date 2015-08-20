69e1c7dd6cebd159082bc0edc577af5cfdea7eb3
futbalguy <kyle.rokita@gmail.com>
2015-08-20 14:23:48 -0700

    code clean-up; commented out console.logs on client-side

--- server/playlists/playlistsUtils.js
+++ server/playlists/playlistsUtils.js
@@ -13,95 +13,65 @@ module.exports = {
   retrievePlaylist: Promise.promisify(function(playlist_id, callback) {
     console.log('retrieving playlist id: ', playlist_id);
     new Playlist({id: playlist_id })
-      .retrievePlaylist()
-      .then(function(found) {
-        if (found) {
-          callback(null, found);
-        } else {
-          console.log('playlist_id not found:' + playlist_id);
-          callback(new Error('Playlist not found'));
-        }
-      })
-      .catch(function(error) {
-        console.log('error:', error);
-        callback(error);
-      });
+    .retrievePlaylist().then(function(found) {
+      if (found) {
+        callback(null, found);
+      } else {
+        console.log('playlist_id not found:' + playlist_id);
+        callback(new Error('Playlist not found'));
+      }
+    }).catch(function(err) {callback(err);});
   }),
 
   //update a playlist in DB by ID
   updatePlaylist: Promise.promisify(function(playlist_id, playlistInfo, callback) {
     playlistMedias = playlistInfo.medias;
 
-    new Playlist({id: playlist_id}).fetch()
-      .then(function(found) {
-        if (found) {
-          found.set('name', playlistInfo.name);
-          if (playlistInfo.current_media_index) {
-            found.set('current_media_index', playlistInfo.current_media_index);
-            console.log('current media index set to: ', found.get('current_media_index'));
-          }
-          found.save()
-          .then( function ( playlist ){
-            new Media_Playlists().query({where: {playlist_id: found.id}}).fetch()
-            .then(function (media_playlists) {
-              return media_playlists.invokeThen('destroy');
-            })
-            .then( function ( empty_playlists ) {
-            // check all the songs exists in the db
-              return new Medias(playlistMedias).mapThen( function (media, index) {
-                return media.fetch().then(function (exist) {
-                  if (!exist) {
-                    media.save().then(function( newMedia ) {
-                      return  new Media_Playlist({playlist_id: playlist_id, media_id: newMedia.get('id'), media_order: index+1}).save();
-                    });
-                  }
-                  else {
-                    return new Media_Playlist({playlist_id: playlist_id, media_id: media.get('id'), media_order: index+1}).save();
-                  }
-                });
-              })
-              .then(function ( medias ) {
-                return new Playlist({id: playlist_id}).retrievePlaylist();
-              })
-              .then(function ( playlists ) {
-                  callback(null, playlists);
-              })
-              .catch(function(error) {
-                console.log('error:', error);
-                callback(error);
+    new Playlist({id: playlist_id}).fetch().then(function(found) {
+      if (found) {
+        found.set('name', playlistInfo.name);
+        if (playlistInfo.current_media_index) {
+          found.set('current_media_index', playlistInfo.current_media_index);
+          console.log('current media index set to: ', found.get('current_media_index'));
+        }
+        found.save().then(function(playlist){
+          new Media_Playlists().query({where: {playlist_id: found.id}}).fetch().then(function(media_playlists) {
+            return media_playlists.invokeThen('destroy');
+          }).then(function(empty_playlists) {
+          // check all the songs exists in the db
+            return new Medias(playlistMedias).mapThen(function(media, index) {
+              return media.fetch().then(function(exist) {
+                if (!exist) {
+                  media.save().then(function(newMedia) {
+                    return new Media_Playlist({playlist_id: playlist_id, media_id: newMedia.get('id'), media_order: index+1}).save();
+                  });
+                }
+                else {
+                  return new Media_Playlist({playlist_id: playlist_id, media_id: media.get('id'), media_order: index+1}).save();
+                }
               });
-            })
-            .catch(function(error) {
-              console.log('error:', error);
-              callback(error);
-            });
-          })
-          .catch(function(error) {
-            console.log('error:', error);
-            callback(error);
-          });
-        }
-        else {
-          console.log('playlist_id not found:' + playlist_id);
-          callback(new Error("Playlist not found"));
-        }
-      })
-      .catch(function(error) {
-        console.log('error:', error);
-        callback(error);
-      });
+            }).then(function(medias) {
+              return new Playlist({id: playlist_id}).retrievePlaylist();
+            }).then(function(playlists) {
+              callback(null, playlists);
+            }).catch(function(error) {callback(err);});
+          }).catch(function(err) {callback(err);});
+        }).catch(function(err) {callback(err);});
+      }
+      else {
+        console.log('playlist_id not found:' + playlist_id);
+        callback(new Error("Playlist not found"));
+      }
+    }).catch(function(err) {callback(err);});
   }),
 
   //store a new playlist in DB
   storePlaylist: Promise.promisify(function(user_id, playlist, callback) {
     var playlistName = playlist.name;
-    console.log('playlistname: ', playlistName);
-
     new Playlist({
       name: playlistName,
       user_id: user_id
-    }).save()
-    .then(function(newPlaylist) {
+    }).save().then(function(newPlaylist) {
       if (!newPlaylist) return callback(new Error('no new playlist'));
       var singleton = require('../singleton.js');
       var user = singleton.users.get(user_id);
@@ -109,31 +79,21 @@ module.exports = {
       return user.setCurrentPlaylist(newPlaylist.get('id')).then(function(user) {
         if (!user) return callback(new Error('Check 2: no user found in new playlist'));
         callback(null, newPlaylist);
-      })
-      .catch(function(error) {
-        console.log('error:', error);
-        callback(error);
-      });
+      }).catch(function(err) {callback(err);});
     })
     .then(function(user) {
       if (!user) return callback(new Error('check 3: no user found in new playlist'));
       callback(null, newPlaylist);
-    })
-    .catch(function(error) {
-      console.log('error:', error);
-      callback(error);
-    });
+    }).catch(function(err) {callback(err);});
   }),
 
-  deletePlaylist: Promise.promisify(function (playlist_id, callback) {
-    new Media_Playlists().query({where: {playlist_id: playlist_id}}).fetch()
-    .then(function (playlist) {
+  deletePlaylist: Promise.promisify(function(playlist_id, callback) {
+    new Media_Playlists().query({where: {playlist_id: playlist_id}}).fetch().then(function(playlist) {
       return playlist.invokeThen('destroy');
-    })
-    .then(function(){
+    }).then(function(){
       return new Playlist({id: playlist_id}).destroy();
     }).then( function () {
       callback();
-    });
+    }).catch(function(err) {callback(err);});
   })
 };
--- server/rooms/roomsUtils.js
+++ server/rooms/roomsUtils.js
@@ -22,100 +22,75 @@ module.exports = {
 
   addRoom: Promise.promisify(function(room, callback) {
     var roomname = room.name;
-    console.log('roomname: ', roomname);
+    
     new Room({
-        name: roomname
-      }).fetch().then(function(found) {
-        if (found) {
-          console.log('room already found:', name);
-          callback(new Error('Room already exists'));
-        } else {
-          console.log("Creating new room: " + roomname);
-          new Room({
-            name: roomname
-          }).save()
-          .then(function(newRoom) {
-            var singleton = require('../singleton.js');
-            newRoom.setSocket(singleton.sockets);
-            singleton.rooms.add(newRoom);
-            callback(null, newRoom);
-          }).catch(function(err) {return callback(err);});
-        }
-      }).catch(function(err) {return callback(err);});
+      name: roomname
+    }).fetch().then(function(found) {
+      if (found) {
+        console.log('room already found:', name);
+        callback(new Error('Room already exists'));
+      } else {
+        console.log("Creating new room: " + roomname);
+        new Room({
+          name: roomname
+        }).save().then(function(newRoom) {
+          var singleton = require('../singleton.js');
+          newRoom.setSocket(singleton.sockets);
+          singleton.rooms.add(newRoom);
+          callback(null, newRoom);
+        }).catch(function(err) {return callback(err);});
+      }
+    }).catch(function(err) {return callback(err);});
   }),
 
   //get a room from DB by ID
   retrieveRoom: Promise.promisify(function(room_id, callback) {
     room_id = parseInt(room_id);
+
     new Room({
-        id: room_id
-      }).fetch({
-        //add related data we would like to return in the withRelated array
-        withRelated: [],
-        require: true
-      }).then(function(found) {
-        if (found) {
-          // var roomWithJoins = found;
-
-          // this is an example of how to add related data to the response object
-          // roomWithJoins.events = [];
-          // found.relations.events.forEach(function(item) {
-          //    roomWithJoins.events.push(cleanAttributes(item.attributes));
-          // });
-
-          callback(null, found);
-        } else {
-          console.log('room_id not found:' + room_id);
-        }
-      }).catch(function(err) {return callback(err);});
+      id: room_id
+    }).fetch({
+      //add related data we would like to return in the withRelated array
+      withRelated: [],
+      require: true
+    }).then(function(found) {
+      if (found) {
+        callback(null, found);
+      } else {
+        console.log('room_id not found:' + room_id);
+      }
+    }).catch(function(err) {return callback(err);});
   }),
 
   retrieveAllRooms: Promise.promisify(function(callback) {
-    new Rooms().fetch()
-    .then(function(found) {
-        if (found) {
-          console.log('found all rooms');
-          // var roomsWithJoins = found;
-
-          // this is an example of how to add related data to the response object
-          // roomWithJoins.events = [];
-          // found.relations.events.forEach(function(item) {
-          //    roomWithJoins.events.push(cleanAttributes(item.attributes));
-          // });
-
-          callback(null, found);
-        } else {
-          console.log('rooms not found');
-        }
-      }).catch(function(err) {return callback(err);});
+    new Rooms().fetch().then(function(found) {
+      if (found) {
+        console.log('found all rooms');
+        callback(null, found);
+      } else {
+        console.log('rooms not found');
+      }
+    }).catch(function(err) {return callback(err);});
   }),
 
   //update a room in DB by ID
   updateRoom: Promise.promisify(function(room_id, roomInfo, callback) {
     new Room({
-        id: room_id
-      }).fetch().then(function(found) {
-        if (found) {
-            found.set(roomInfo).save()
-            .then(function(updatedRoom) {
-              callback(null, updatedRoom);
-            })
-            .catch(function(error) {
-              console.log('error:', error);
-            });
-        } else {
-          callback(new Error("No room found"));
-        }
-      }).catch(function(err) {
-        return callback(err);
-      });
+      id: room_id
+    }).fetch().then(function(found) {
+      if (found) {
+        found.set(roomInfo).save().then(function(updatedRoom) {
+          callback(null, updatedRoom);
+        }).catch(function(err) {return callback(err);});
+      } else {
+        callback(new Error("No room found"));
+      }
+    }).catch(function(err) {return callback(err);});
   }),
 
   setSocketsForTimer: function(sockets) {
     console.log('setting sockets for timer');
     openSockets = sockets;
-    // var singleton = require('../singleton.js');
-    // singleton.sockets = sockets;
   },
 
   getSocketsForTimer: function() {
